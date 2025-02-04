ce8ef6f0997d5a761a0086ca5f2cf1d4a20e9692
Vinay Buddhi <vinaysbuddhi@gmail.com>
2025-02-04 14:42:09 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144586;
+let accountEmail = "hitesh@gmail.com";
+var accountPassword = "1425";
+accountCity = "Jaipur";
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
