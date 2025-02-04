30473d38e0a112a4693b7526400e2504328d186e
Vinay Buddhi <vinaysbuddhi@gmail.com>
2025-02-04 16:40:19 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,3 +6,30 @@ accountCity = "Jaipur";
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+let age = 46;
+
+age = 24;
+
+age = 30;
+
+console.log(age);
+
+const MYAGE = 25;
+
+const a = 10;
+
+console.log(a);
+
+var x = 45; 
+
+// block scope of let variables
+let b = false;
+
+{
+    let a = 60;
+    let b = true;
+    console.log(b);
+}
+
+console.log(b);
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,67 @@
+// Primitive datatypes in JS
+// Number, String, Boolean, Undefined, Null, BigInt, Symbol
+
+// Number datatype is a Primitive variable to store numbers :
+let age = 24;
+console.log(age);
+console.log(typeof age);
+
+// Number datatype example for floating point numbers :
+let price = 100.50;
+console.log(price);
+console.log(typeof price);
+
+// String datatype "****" is a Primitive variable to store string or character's :
+let fullName = "vinay";
+console.log(fullName);
+console.log(typeof fullName);
+
+// Boolean datatype true or false is a Primitive variable to store boolean expression True or False :
+isFollow = true;
+console.log(isFollow);
+console.log(typeof isFollow);
+
+// Undefined datatype it is a Primitive variable its value is not known :
+let x;
+console.log(x);
+console.log(typeof x);
+
+// Null datatype is a object datatype it is a primitive datatype to specify null value in a variable :
+let y = null;
+console.log(y);
+console.log(typeof y);
+
+// BigInt datatype is a Primitive variable to store big integer values :
+let num = BigInt("123");
+console.log(num);
+console.log(typeof num);
+
+// symbol datatype is a Primitive variable :
+let Name = Symbol("Hello!");
+console.log(Name);
+console.log(typeof Name);
+
+
+// Non Primitive datatype consists of objects
+// Object is collection of values stored in key-value pairs like key:value start and end in { } braces
+
+const student = {
+    fullName : "Rahul Kumar",
+    age : 20,
+    cgpa : 8.2,
+    isPass : true
+};
+
+console.log(student);
+console.log(typeof student);
+
+// accessing the keys in objects
+console.log(student["fullName"]);
+console.log(student.age);
+
+// To update the variable values or key-value pairs in objects
+student["age"] = student["age"] + 1;
+console.log(student["age"]);
+
+student["fullName"] = "Vinay";
+console.log(student["fullName"]);
